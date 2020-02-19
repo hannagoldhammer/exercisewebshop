88d4756b416917b0e763b76377016f4abaa2e090
Hanna <[email address]>
2020-02-19 11:19:16 +0100

    Mongoose added

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // Härifrån startar vi upp vår webshop 
-const {app, port} = require("./src/server")
+const {app, port, config} = require("./src/server")
+const mongoose = require("mongoose")
 
 
-// Starta igång servern
-const server = app.listen(port, () => {
-    console.log(`App listenning on port ${port}`)
+
+// För att undvika error i terminalen när man använder mongoose.
+const options = {
+    useUnifiedTopology: true, 
+    useNewUrlParser: true
+}
+
+// Connecta till mongoose och starta sedan applikationen 
+mongoose.connect(config.databaseURL, options).then(() => {
+    console.log(`You are successfully connected to port: ${port}`);
+    app.listen(port)
 })
--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@
 const express = require("express");
 const app = express();
 const port = 8080
+const config = require("../config/config")
+const mongoose = require("mongoose")
 
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
 
 
-module.exports = {app, port}
+module.exports = {app, port, config};
